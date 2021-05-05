a01fbd88e426e5d2b17ce574e82ea05417b57ba4
Fidel C <coronel08@aol.com>
2021-05-05 16:07:11 -0700

    added sms participants to conversation

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # twilio_update_ip
 Python Script to update twilio inside, will add twilio script to get text messaging api going as well.
 
+[Messages api](https://www.twilio.com/docs/sms/api/message-resource#create-a-message-resource)
+
 ### Todo
 * Setup cron job to run update ip script
 * Setup Twilio CLI on server to process text messages and conversations on server. [Twilio CLI](https://www.twilio.com/docs/twilio-cli/quickstart)
   * Tie into [WhatsApp](https://www.twilio.com/docs/conversations/using-whatsapp-conversations) and [quick start](https://www.twilio.com/docs/conversations/quickstart)
   * [x] Create a WhatsApp business account for 1(323)622-4366
     * [autopilot whatsapp](https://www.twilio.com/docs/autopilot/channels/whatsapp) and [WhatsApp Senders](https://www.twilio.com/console/sms/whatsapp/senders) and [autopilot all](https://www.twilio.com/docs/autopilot/channels) 
-  * Create a FB business manager account (FB Business)[https://business.facebook.com/overview]
+  * Create a FB business manager account [FB Business](https://business.facebook.com/overview)
+  * [WhatsApp API](https://www.twilio.com/docs/whatsapp/api)
--- sms_test.py
+++ sms_test.py
@@ -8,6 +8,8 @@ load_dotenv(find_dotenv())
 account_sid = os.environ.get("twilio_sid")
 auth_token = os.environ.get("twilio_token")
 client = Client(account_sid, auth_token)
+fidel_phone = '+13236333898'
+twilio_phone = '+13236224366'
 
 
 def send_text(body='test'):
@@ -15,9 +17,9 @@ def send_text(body='test'):
     Sends a text message to me, if nothing is passed in function it will send test
     """
     message = client.messages.create(
-        to='+13236333898',
-        from_='+13236224366',
-        body= body
+        to=fidel_phone,
+        from_=twilio_phone,
+        body=body
     )
     print(message.sid, message.to, message.body)
 
@@ -27,7 +29,8 @@ def fetch_messages():
     Fetches the last 30 text messages, before 2021
     """
     # can add filter like from_= , to= , date_sent=datetime(2021,1,1,0,0,0) , date_sent_after=datetime()
-    messages = client.messages.list(date_sent_before=datetime(2021, 1, 1, 0, 0, 0),limit=30)
+    messages = client.messages.list(
+        date_sent_before=datetime(2021, 1, 1, 0, 0, 0), limit=30)
     for record in messages:
         print(record.sid, '\n', record.to, record.body, '\n')
     # Fetch messages by id
@@ -35,12 +38,41 @@ def fetch_messages():
 
 
 def start_conversation():
+    """ Create a conversation and return conversation_sid """
     conversation = client.conversations \
         .conversations  \
         .create(friendly_name='My First Conversation')
     print(conversation.sid)
 
 
+def fetch_conversation(conversation_sid):
+    """ Fetch the conversation using sid and return conversation_service """
+    conversation = client.conversations \
+        .conversations(conversation_sid) \
+        .fetch()
+    print(conversation.chat_service_sid)
 
-# send_text("THis is a test run")
+
+def add_conversation(conversation_sid):
+    participant = client.conversations \
+        .conversations(conversation_sid) \
+        .participants \
+        .create(
+            messaging_binding_address=fidel_phone,
+            messaging_binding_proxy_address=twilio_phone
+        )
+    print(participant.sid)
+
+
+# send_text("This is a test run")
 # fetch_messages()
+
+
+# start_conversation()
+# from start conversation
+conversation_sid = 'CHa58ccfd9e9a148cd91b32b2989f97bf9'
+# from fetch conversation
+conversation_service = 'ISe762ba9737c643c7993f602847ebe2d6'
+# fetch_conversation(conversation_sid)
+participant_sid = 'MB980856734004444b81b3ea640953d9e4'
+add_conversation(conversation_sid)
